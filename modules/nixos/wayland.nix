c7d2d9dbe2df88b3f5c9931d9942cd031e86b657
sad <418@duck.com>
2022-09-26 02:42:11 +0300

    bring back fonts

--- modules/nixos/wayland.nix
+++ modules/nixos/wayland.nix
@@ -21,6 +21,60 @@
       };
     };
 
+    # TODO: move it to themes later
+    # Icons MS Nerdfont Icons override
+    fonts = {
+      fontDir.enable = true;
+      enableGhostscriptFonts = true;
+      fonts = with pkgs; [
+        # font-awesome dejavu_fonts symbola corefonts
+
+        # icon fonts
+        material-icons
+        material-design-icons
+
+        # normal fonts
+        source-code-pro
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        roboto
+
+        (nerdfonts.override {
+          fonts = [
+            "FiraCode"
+            "JetBrainsMono"
+          ];
+        })
+      ];
+
+      # use fonts specified by user rather than default ones
+      enableDefaultFonts = false;
+
+      # REVIEW: is it false or grue by default? Do i need it with below strings?
+      # fontconfig.enable = true; # Allow fonts to be discovered
+
+      # user defined fonts
+      # the reason there's Noto Color Emoji everywhere is to override DejaVu's
+      # B&W emojis that would sometimes show instead of some Color emojis
+      fontconfig.defaultFonts = {
+        serif = ["Noto Serif" "Noto Color Emoji"];
+        sansSerif = ["Noto Sans" "Noto Color Emoji"];
+        monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+        emoji = ["Noto Color Emoji"];
+      };
+
+      # somwehere in above 'let .. in' construct:
+      # fontList = [ "Sarasa Mono SC" "FantasqueSansMono Nerd Font Mono" ];
+      # .. and then here maybe something like
+      # fontconfig.defaultFonts = {
+      #   monospace = fontList;
+      #   sansSerif = fontList;
+      #   serif = fontList;
+      # };
+
+    };
+
     # This will allow brightness control from users in the video group.
     user.extraGroups = [ "video" ];
     hardware.brillo.enable = true;
