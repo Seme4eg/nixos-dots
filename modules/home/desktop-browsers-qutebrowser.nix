d8c25b77828c300188a643f96f16397b915ff924
sad <418@duck.com>
2022-10-11 23:42:07 +0300

    formatting

--- modules/home/desktop-browsers-qutebrowser.nix
+++ modules/home/desktop-browsers-qutebrowser.nix
@@ -9,7 +9,10 @@ let cfg = config.modules.desktop.browsers.qutebrowser;
 in {
   options.modules.desktop.browsers.qutebrowser = {
     enable = lib.mkEnableOption "qutebrowser";
-    userStyles = lib.mkOption { type = lib.types.lines; default = ""; };
+    userStyles = lib.mkOption {
+      type = lib.types.lines;
+      default = "";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -19,25 +22,25 @@ in {
       # quickmarks = { ... }; # TODO: work for sops-nix
     };
 
-    home.packages = with pkgs; [
+    home.packages = with pkgs;
+      [
+        (python3.withPackages (p:
+          with p; [
+            # For Brave adblock in qutebrowser, which is significantly better than the
+            # built-in host blocking. Works on youtube and crunchyroll ads!
+            adblock
 
-      (python3.withPackages (p: with p; [
-        # For Brave adblock in qutebrowser, which is significantly better than the
-        # built-in host blocking. Works on youtube and crunchyroll ads!
-        adblock
+            # For userscript code-select to use newline as delimeter, nor ';'
 
-        # For userscript code-select to use newline as delimeter, nor ';'
-
-        # (Suggestion from discord server):
-        # In that case, the standard solution is to make a flake.nix/default.nix
-        # with buildPythonApplication/buildPythonPackage You can also just use
-        # patchShebangs
-        pyperclip # TODO: doesn't work
-        # userScriptsPatchShebang = # this also doesn't work
-        #   ''patchShebangs ${inputs.self}/config/qutebrowser/userscripts'';
-      ]))
-
-    ];
+            # (Suggestion from discord server):
+            # In that case, the standard solution is to make a flake.nix/default.nix
+            # with buildPythonApplication/buildPythonPackage You can also just use
+            # patchShebangs
+            pyperclip # TODO: doesn't work
+            # userScriptsPatchShebang = # this also doesn't work
+            #   ''patchShebangs ${inputs.self}/config/qutebrowser/userscripts'';
+          ]))
+      ];
 
     xdg.configFile = {
       "qutebrowser" = {
@@ -46,7 +49,7 @@ in {
       };
     };
 
-    # TODO: i guess it's for theming?
+    # TODO
     # xdg.dataFile."qutebrowser/userstyles.css".text = cfg.userStyles;
 
     # Run scripts during rebuild/switch
@@ -56,11 +59,13 @@ in {
         ''[ ! -d "$HOME/Downloads" ] && mkdir $HOME/Downloads;'';
 
       # Install language dictionaries for spellcheck backends
-      qbInstallDicts = lib.concatStringsSep "\\\n" (map (lang: ''
-          if ! find "${config.xdg.dataHome}/qutebrowser/qtwebengine_dictionaries" -type d -maxdepth 1 -name "${lang}*" 2>/dev/null | grep -q .; then
-            ${pkgs.python3}/bin/python ${pkgs.qutebrowser}/share/qutebrowser/scripts/dictcli.py install ${lang}
-          fi
-        '') [ "en-US" "ru-RU" ]);
+      qbInstallDicts = lib.concatStringsSep ''
+        \
+      '' (map (lang: ''
+        if ! find "${config.xdg.dataHome}/qutebrowser/qtwebengine_dictionaries" -type d -maxdepth 1 -name "${lang}*" 2>/dev/null | grep -q .; then
+          ${pkgs.python3}/bin/python ${pkgs.qutebrowser}/share/qutebrowser/scripts/dictcli.py install ${lang}
+        fi
+      '') [ "en-US" "ru-RU" ]);
     };
   };
 }
